30e081e96e8745a97f6fa802eb5244e95cf40c9f
shaziya01 <sk.shaziya9082@gmail.com>
2025-03-01 11:20:07 +0530

    Added ImportExport fields in admin.py

--- kct/admin.py
+++ kct/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
 from import_export import resources 
-from .models import SystemMaster, BeneficiaryCategory, ManagingListCategoryMaster, ListItemCategory, DropdownOption, EventMaster
+from .models import BeneficiaryAid, CaseStudiesMaster, KCTEnquireMaster, KeyProgramMaster, LatestEventMaster, SystemMaster, BeneficiaryCategory, ManagingListCategoryMaster, ListItemCategory, DropdownOption, EventMaster, SystemMasterCategory
 from import_export.admin import ImportExportModelAdmin
+
+
+
+
+class SystemMasterCategoryresources(resources.ModelResource):
+    class Meta:
+        model = SystemMasterCategory
+        import_id_fields = ['id']
+        fields = ['id', 'name']
+
+
+class SystemMasterCategoryAdmin(ImportExportModelAdmin):
+    resource_class = SystemMasterCategoryresources
+    list_display = ['id', 'name']
+    search_fields = ['id','name']
+
+admin.site.register(SystemMasterCategory,SystemMasterCategoryAdmin)
+
 
 
 class SystemMasterresources(resources.ModelResource):
     class Meta:
         model = SystemMaster
         import_id_fields = ['id']
-        fields = ['id', 'category', 'system_name', 'system_title', 'system_value','is_active']
+        fields = ['id', 'category', 'system_name', 'system_title', 'system_value', 'system_img', 'is_active']
 
 
 class SystemMasterAdmin(ImportExportModelAdmin):
@@ -24,7 +42,7 @@ class EventMasterresources(resources.ModelResource):
     class Meta:
         model = EventMaster
         import_id_fields = ['id'] 
-        fields = ['id', 'event_title', 'event_description', 'is_active']  
+        fields = ['id', 'event_title', 'event_description','event_feature', 'event_img', 'reactions', 'time', 'order', 'is_active']  
 class EventMasterAdmin(ImportExportModelAdmin):
     # list_display = ('event_title','event_description','is_active')
     resource_class = EventMasterresources
@@ -43,32 +61,110 @@ class BeneficiaryCategoryresources(resources.ModelResource):
     class Meta:
         model = BeneficiaryCategory
         import_id_fields = ['id'] 
-        fields = ['id', 'name', 'number', 'is_active']  
+        fields = ['id', 'name', 'icon', 'number', 'has_dropdown', 'is_active']  
 class BeneficiaryCategoryAdmin(ImportExportModelAdmin):
     resource_class = BeneficiaryCategoryresources
     list_display = ['id', 'name', 'number', 'is_active']
     search_fields = ['id','name']
 
 admin.site.register(BeneficiaryCategory,BeneficiaryCategoryAdmin)  
+ 
 
 
-  
+ 
+
+class dropdownoptions(resources.ModelResource):
+    class Meta:
+        model = DropdownOption
+        import_id_fields = ['id']
+        fields = ['id','category', 'name', 'is_active']
+
+class dropdownoptionsAdmin(ImportExportModelAdmin):
+    resource_class = dropdownoptions
+    list_display = ['id','category', 'name', 'is_active']
+    search_fields = ['id', 'name']
+admin.site.register(DropdownOption, dropdownoptionsAdmin)
 
 
 
-class ManagingList(resources.ModelResource):
+
+class KeyProgramMasterresource(resources.ModelResource):
+    class Meta:
+        model = KeyProgramMaster
+        import_id_fields = ['id']
+        fields = ['id','system', 'is_active']
+
+class KeyProgramMasterAdmin(ImportExportModelAdmin):
+    resource_class = KeyProgramMasterresource
+    list_display = ['id','system', 'is_active']
+    search_fields = ['id', 'name']
+admin.site.register(KeyProgramMaster, KeyProgramMasterAdmin)
+
+
+
+
+class BeneficiaryAidresource(resources.ModelResource):
+    class Meta:
+        model = BeneficiaryAid
+        import_id_fields = ['id']
+        fields = ['id', 'system', 'is_active']
+
+class BeneficiaryAidAdmin(ImportExportModelAdmin):
+    resource_class = BeneficiaryAidresource
+    list_display = ['id','system', 'is_active']
+    search_fields = ['id', 'name']
+admin.site.register(BeneficiaryAid, BeneficiaryAidAdmin)
+
+
+
+class CaseStudiesMasterresource(resources.ModelResource):
+    class Meta:
+        model = CaseStudiesMaster
+        import_id_fields = ['id']
+        fields = ['id', 'system', 'is_active']
+
+class CaseStudiesMasterAdmin(ImportExportModelAdmin):
+    resource_class = CaseStudiesMasterresource
+    list_display = ['id','system', 'is_active']
+    search_fields = ['id', 'name']
+admin.site.register(CaseStudiesMaster, CaseStudiesMasterAdmin)
+
+
+
+
+
+
+class LatestEventMasterresource(resources.ModelResource):
+    class Meta:
+        model = LatestEventMaster
+        import_id_fields = ['id']
+        fields = ['id', 'system', 'is_active']
+
+class LatestEventMasterAdmin(ImportExportModelAdmin):
+    resource_class = LatestEventMasterresource
+    list_display = ['id','system', 'is_active']
+    search_fields = ['id', 'name']
+admin.site.register(LatestEventMaster, LatestEventMasterAdmin)
+
+
+
+
+
+class ManagingListCategoryresources(resources.ModelResource):
     class Meta:
         model = ManagingListCategoryMaster
         import_id_fields = ['id']
         fields = ['id', 'title', 'is_active']
         
-class ManagingListAdmin(ImportExportModelAdmin):
-    resource_class = ManagingList
+class ManagingListCategoryAdmin(ImportExportModelAdmin):
+    resource_class = ManagingListCategoryresources
     list_display = ['id', 'title', 'is_active']
     search_fields = ['id', 'title']
 
 
-admin.site.register(ManagingListCategoryMaster, ManagingListAdmin)  
+admin.site.register(ManagingListCategoryMaster, ManagingListCategoryAdmin) 
+
+
 
 
 
@@ -78,7 +174,7 @@ class ListItem(resources.ModelResource):
     class Meta:
         model = ListItemCategory
         import_id_fields = ['id']
-        fields = ['name', 'is_active']
+        fields = ['id', 'category', 'name' 'is_active']
 
 
 class ListItemAdmin(ImportExportModelAdmin):
@@ -88,17 +184,19 @@ class ListItemAdmin(ImportExportModelAdmin):
 
 
 admin.site.register(ListItemCategory, ListItemAdmin) 
- 
 
-class dropdownoptions(resources.ModelResource):
+
+
+
+
+class KCTEnquireMasterresource(resources.ModelResource):
     class Meta:
-        model = DropdownOption
+        model = KCTEnquireMaster
         import_id_fields = ['id']
-        fields = ['category', 'name', 'is_active']
+        fields = ['id', 'name', 'email', 'phone', 'message', 'is_active']
 
-class dropdownoptionsAdmin(ImportExportModelAdmin):
-    resource_class = dropdownoptions
-    list_display = ['id','category', 'name', 'is_active']
-    search_fields = ['id', 'name']
-admin.site.register(DropdownOption, dropdownoptionsAdmin)
-
+class KCTEnquireMasterAdmin(ImportExportModelAdmin):
+    resource_class = KCTEnquireMasterresource
+    list_display = ['id','name', 'email', 'phone', 'message', 'is_active']
+    search_fields = ['id', 'name', 'email', 'phone']
+admin.site.register(KCTEnquireMaster, KCTEnquireMasterAdmin)
